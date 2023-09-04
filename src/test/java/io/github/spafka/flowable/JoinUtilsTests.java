eae6b176feb4d1da41b065784b715d32c70330fb
spafka <spafka@163.com>
2023-09-05 01:00:55 +0800

    use myway back

---
+++ src/test/java/io/github/spafka/flowable/JoinUtilsTests.java
@@ -0,0 +1,141 @@
+package io.github.spafka.flowable;
+
+import io.github.spafka.flowable.core.JoinUtils;
+import io.github.spafka.util.Utils;
+import io.vavr.Tuple2;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class JoinUtilsTests {
+
+    @Test
+    public void _1() {
+
+
+        {
+            List<Integer> collect = IntStream.rangeClosed(1, 10000).boxed().collect(Collectors.toList());
+
+
+            Utils.timeTakeMS(() -> {
+                List<Object> objects = JoinUtils.sortJoin(collect, collect, Integer::compareTo, Integer::compareTo, Integer::compareTo, (a, b) -> a);
+                System.out.println();
+            }, "new join");
+        }
+
+
+        {
+            List<Integer> collect = IntStream.rangeClosed(1, 10000).boxed().collect(Collectors.toList());
+
+
+            Utils.timeTakeMS(() -> {
+                List<Object> objects = JoinUtils.sortJoin(collect, collect, Integer::compareTo, Integer::compareTo, Integer::compareTo, (a, b) -> a);
+                System.out.println();
+                }, "new join");
+        }
+
+        {
+            List<Integer> collect = Arrays.asList(1,1);
+            List<Integer> collect2 = Arrays.asList(1,1);
+
+            Utils.timeTakeMS(() -> {
+                List<Object> objects = JoinUtils.sortJoin(collect, collect, Integer::compareTo, Integer::compareTo, Integer::compareTo, (a, b) -> a);
+                System.out.println();
+            }, "new join");
+
+        }
+
+    }
+
+    @Test
+    public void _2() {
+
+
+        {
+            List<Integer> list1 = IntStream.rangeClosed(2, 3).boxed().collect(Collectors.toList());
+            List<Integer> list2 = IntStream.rangeClosed(1, 1).boxed().collect(Collectors.toList());
+
+
+            Utils.timeTakeMS(() -> {
+                List<Tuple2<Integer, Integer>> tuple2s = JoinUtils.sortLeftJoin(list1, list2, Integer::compareTo, Integer::compareTo, Integer::compareTo, (a, b) -> new Tuple2<>(a, b));
+                System.out.println(tuple2s);
+            }, "new join");
+        }
+
+        {
+            List<Integer> list1 = Arrays.asList(1,3,3,4,5,6);
+            List<Integer> list2 = Arrays.asList(1,3,5);
+
+
+            Utils.timeTakeMS(() -> {
+                List<Tuple2<Integer, Integer>> tuple2s = JoinUtils.sortLeftJoin(list1, list2, Integer::compareTo, Integer::compareTo, Integer::compareTo, (a, b) -> new Tuple2<>(a, b));
+                System.out.println(tuple2s);
+            }, "new join");
+        }
+
+        {
+            List<Integer> list1 = Arrays.asList(1,3,3,4,5,6);
+            List<Integer> list2 = Arrays.asList(1);
+
+
+            Utils.timeTakeMS(() -> {
+                List<Tuple2<Integer, Integer>> tuple2s = JoinUtils.sortLeftJoin(list1, list2, Integer::compareTo, Integer::compareTo, Integer::compareTo, (a, b) -> new Tuple2<>(a, b));
+                System.out.println(tuple2s);
+            }, "new join");
+        }
+
+        {
+            List<Integer> list1 = Arrays.asList(7,3,3,4,5,6);
+            List<Integer> list2 = Arrays.asList(1,3,3);
+
+
+            Utils.timeTakeMS(() -> {
+                List<Tuple2<Integer, Integer>> tuple2s = JoinUtils.sortLeftJoin(list1, list2, Integer::compareTo, Integer::compareTo, Integer::compareTo, (a, b) -> new Tuple2<>(a, b));
+                System.out.println(tuple2s);
+            }, "new join");
+        }
+
+        {
+            List<Integer> list1 = Arrays.asList(7,9,0);
+            List<Integer> list2 = Arrays.asList(1,3,3);
+
+
+            Utils.timeTakeMS(() -> {
+                List<Tuple2<Integer, Integer>> tuple2s = JoinUtils.sortLeftJoin(list1, list2, Integer::compareTo, Integer::compareTo, Integer::compareTo, (a, b) -> new Tuple2<>(a, b));
+                System.out.println(tuple2s);
+            }, "new join");
+        }
+
+        {
+            List<Integer> list1 = Arrays.asList(3,3,3,3);
+            List<Integer> list2 = Arrays.asList(1,3,3);
+
+
+            Utils.timeTakeMS(() -> {
+                List<Tuple2<Integer, Integer>> tuple2s = JoinUtils.sortLeftJoin(list1, list2, Integer::compareTo, Integer::compareTo, Integer::compareTo, (a, b) -> new Tuple2<>(a, b));
+                System.out.println(tuple2s);
+            }, "new join");
+        }
+
+
+
+    }
+
+    @Test
+    public void _3(){
+        {
+            List<Integer> list1 = Arrays.asList(3,3,3,3);
+            List<Integer> list2 = Arrays.asList(1,3,3);
+
+
+            Utils.timeTakeMS(() -> {
+                List<Tuple2<Integer, Integer>> tuple2s = JoinUtils.sortLeftJoin(list1, list2, Integer::compareTo, Integer::compareTo, Integer::compareTo, (a, b) -> new Tuple2<>(a, b));
+                System.out.println(tuple2s);
+            }, "new join");
+        }
+
+    }
+}
--- src/test/java/io/github/spafka/flowable/Sorts.java
+++ src/test/java/io/github/spafka/flowable/Sorts.java
@@ -14,24 +14,17 @@ public class Sorts {
 
         ListMultimap<Integer, String> multiMap = ArrayListMultimap.create();
         // 添加键值对到多重映射
-        multiMap.put(1, "apple");
-        multiMap.put(1, "banana");
-        multiMap.put(1, "cherry");
-        multiMap.put(2, "dog");
-        multiMap.put(2, "cat");
-        multiMap.put(2, "elephant");
-        multiMap.put(3, "car");
-        multiMap.put(3, "bicycle");
-        multiMap.put(3, "train");
+
 
         // 使用Ordering对多重映射进行排序
 
 
-        Ordering<Map.Entry<Integer, String>> ordering = Ordering.natural().onResultOf(Map.Entry::getKey);
+        Ordering<Map.Entry<Integer, String>> ordering
+                = Ordering.natural().onResultOf(Map.Entry::getKey);
         List<Map.Entry<Integer, String>> sortedEntries = ordering.sortedCopy(multiMap.entries());
 
 
-        sortedEntries.
+
         // 打印排序后的多重映射
         for (Map.Entry<Integer, String> entry : sortedEntries) {
             int key = entry.getKey();
